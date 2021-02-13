8142dc52549dc530274949324e846fd824abcd3d
Lunarixus <Nathanb644@hotmail.co.uk>
2021-02-13 07:42:03 +0000

    ginkgo: Remove packages at build time

---
+++ remove_packages/Android.mk
@@ -0,0 +1,40 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := RemovePackages
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES := \
+    AmbientSensePrebuilt \
+    arcore \
+    Camera2 \
+    DevicePolicyPrebuilt \
+    DiagnosticsToolPrebuilt \
+    Drive \
+    GoogleCamera \
+    Maps \
+    MyVerizonServices \
+    OBDM_Permissions \
+    obdm_stub \
+    OemDmTrigger \
+    OPScreenRecord \
+    Ornament \
+    PixelLiveWallpaperPrebuilt \
+    PixelWallpapers2020 \
+    SafetyHubPrebuilt \
+    ScribePrebuilt \
+    Showcase \
+    Snap \
+    SoundAmplifierPrebuilt \
+    SprintDM \
+    SprintHM \
+    VZWAPNLib \
+    VzwOmaTrigger \
+    WallpapersBReel2020 \
+    YouTube \
+    YouTubeMusicPrebuilt
+
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := /dev/null
+include $(BUILD_PREBUILT)
